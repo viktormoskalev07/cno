8a9c5515ead5eb60b21096c615273a9106afb466
Shangri <astasenko@smart-ui.pro>
2021-06-15 18:08:54 +0300

    created slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,107 +1,119 @@
-body=document.querySelector("body");
-html=document.querySelector("html");
-header=document.querySelector("header");
-btnMenu=document.querySelector(".btn-menu");
+body = document.querySelector("body");
+html = document.querySelector("html");
+header = document.querySelector("header");
+btnMenu = document.querySelector(".btn-menu");
 
 
 // мобильное меню
 nav = document.querySelector(".nav");
-document.querySelector(".btn-menu").addEventListener("click" , togglenav);
+document.querySelector(".btn-menu").addEventListener("click", togglenav);
 document.querySelector(".nav-menu").onclick = closenav;
 
 document.querySelector(".nav-background").onclick = closenav;
-function closenav(){
-  nav.classList.remove("nav-open");
-  btnMenu.style.marginRight="0";
-  showscroll();
-  
+
+function closenav() {
+    nav.classList.remove("nav-open");
+    btnMenu.style.marginRight = "0";
+    showscroll();
+
 }
+
 function togglenav() {
-  nav.classList.toggle("nav-open");
-if(nav.classList.contains("nav-open")){
-  btnMenu.style.marginRight=window.innerWidth-html.offsetWidth +"px";
-  hidescroll();
-  
-} else {
-  showscroll();
-  btnMenu.style.marginRight="0";
+    nav.classList.toggle("nav-open");
+    if (nav.classList.contains("nav-open")) {
+        btnMenu.style.marginRight = window.innerWidth - html.offsetWidth + "px";
+        hidescroll();
+
+    } else {
+        showscroll();
+        btnMenu.style.marginRight = "0";
+    }
 }
-}
+
 // мобильное меню
 
 
 // отключить прокрутку
-function hidescroll(){
-  body.style.paddingRight=window.innerWidth-html.offsetWidth +"px";
-  body.style.overflow="hidden";
+function hidescroll() {
+    body.style.paddingRight = window.innerWidth - html.offsetWidth + "px";
+    body.style.overflow = "hidden";
 }
 
-function showscroll(){
-  body.style.paddingRight=0;
-  body.style.overflow="visible"
+function showscroll() {
+    body.style.paddingRight = 0;
+    body.style.overflow = "visible"
 }
 
-let scrolid = 0 ;
-function togglescroll(){
-  if (scrolid==0){
-    body.style.paddingRight=window.innerWidth-html.offsetWidth +"px";
-    body.style.overflow="hidden";
-    scrolid=1;
-  } else{
-    body.style.paddingRight=0;
-    body.style.overflow="visible"
-    scrolid=0;
-  }
+let scrolid = 0;
+
+function togglescroll() {
+    if (scrolid == 0) {
+        body.style.paddingRight = window.innerWidth - html.offsetWidth + "px";
+        body.style.overflow = "hidden";
+        scrolid = 1;
+    } else {
+        body.style.paddingRight = 0;
+        body.style.overflow = "visible"
+        scrolid = 0;
+    }
 }
 
 // отключить прокрутку
 
- 
 
-
-
-
- // Появление меню при прокрутке 
- window.onload= function(){
-  pagescroll(); 
+// Появление меню при прокрутке
+window.onload = function () {
+    pagescroll();
 }
 
-function pagescroll(){ 
-  let pscroll = 0;
-  window.addEventListener('scroll', function() { 
-      if (pscroll<pageYOffset&pageYOffset>header.offsetHeight){
-               header.style.transform="translateY(-100%)"; //прячем хедер при прокрутке вниз            
-       }else { 
-            header.style.transform="none"; //показываем хедер при прокрутке вверх        
-       }  
-   pscroll=pageYOffset;
- });
-}
-
- //Появление меню при прокрутке
-
-//swiper
-  window.addEventListener('load', function(){
-     var swiper = new Swiper(".cards1", {
-      pagination: {
-        el: ".swiper-pagination",
-      },
-      breakpoints: { 
-        680: {
-          slidesPerView: 2,
-          spaceBetween: 20
+function pagescroll() {
+    let pscroll = 0;
+    window.addEventListener('scroll', function () {
+        if (pscroll < pageYOffset & pageYOffset > header.offsetHeight) {
+            header.style.transform = "translateY(-100%)"; //прячем хедер при прокрутке вниз
+        } else {
+            header.style.transform = "none"; //показываем хедер при прокрутке вверх
         }
-      }
-    });  
-  })  
-   
+        pscroll = pageYOffset;
+    });
+}
+
+//Появление меню при прокрутке
 
 //swiper
+window.addEventListener('load', function () {
+    var swiper = new Swiper(".cards1", {
+        pagination: {
+            el: ".swiper-pagination",
+        },
+        breakpoints: {
+            680: {
+                slidesPerView: 2,
+                spaceBetween: 20
+            }
+        }
+    });
+})
+
+window.addEventListener('load', function () {
+    const swiperTabs = new Swiper(".swiper-tabs", {
+        pagination: {
+            el: ".swiper-pagination-tabs",
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 20
+            }
+        }
+    });
+})
+
+    //swiper
 
 
 
-
-//отправка формы  
+//отправка формы
 // let form = document.querySelectorAll('.order-form');
 
 // const modal = document.getElementById("modal");
@@ -138,7 +150,6 @@ function pagescroll(){
 // };
 
 
-
 // for (i = 0; i < form.length; i++) {
 //   form[i].addEventListener('submit', ajaxform);
 // } 
@@ -150,25 +161,25 @@ function pagescroll(){
 // 	window.addEventListener("load", function () {
 // 		const accordion = document.querySelector(".accordion");
 // 		const items = accordion.querySelectorAll(".accordion__item");
-	
+
 // 		items.forEach((item) => {
 // 			const title = item.querySelector(".accordion__title");
-	
+
 // 			title.addEventListener("click", (e) => {
 // 				const opened_item = accordion.querySelector(".is-open");
-	
+
 // 				toggle_item(item);
-	
+
 // 				if (opened_item && opened_item !== item) {
 // 					toggle_item(opened_item);
 // 				}
 // 			});
 // 		});
-	
+
 // 		const toggle_item = (item) => {
 // 			const body = item.querySelector(".accordion__body");
 // 			const content = item.querySelector(".accordion__content");
-	
+
 // 			if (item.classList.contains("is-open")) {
 // 				body.removeAttribute("style");
 // 				item.classList.remove("is-open");
